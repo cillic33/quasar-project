9d1fbb570317cfd1149ff8b9bcc9cafbbf3b13ef
Маргарита Попова <cillic@yandex.ru>
2023-08-05 17:10:33 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quasar-project/',
   plugins: [
     vue({
       template: { transformAssetUrls }
